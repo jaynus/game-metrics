02c092e061f6a3f5eefaeb857f7a16a3d925c38e
jaynus <jaynus@gmail.com>
2020-02-10 09:58:41 -0800

    Add target filtering

--- src/logging.rs
+++ src/logging.rs
@@ -1,13 +1,18 @@
-use log::{LevelFilter, Log, Metadata, Record, SetLoggerError};
+#![allow(unused_variables, dead_code)]
+use crossbeam_channel::{Receiver, Sender};
+use fxhash::FxHashMap;
+use log::{Level, LevelFilter, Log, Metadata, Record, SetLoggerError};
+use parking_lot::Mutex;
 use std::{
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        Arc,
+    },
     thread::JoinHandle,
-    sync::{Arc, atomic::{Ordering, AtomicBool}},
-    cell::RefCell,
 };
-use crossbeam_channel::{Sender, Receiver};
 
 lazy_static::lazy_static! {
-    static ref LOGGER: Arc<Logger> = Arc::new(Logger::new());
+    static ref LOGGER: Arc<Logger> = Arc::new(Logger::default());
 }
 
 struct InternalRecord {
@@ -19,24 +24,36 @@ struct InternalRecord {
     line: Option<u32>,
 }
 
-pub struct LoggerSettings {
-
+#[derive(Default)]
+pub struct Settings {
+    targets: FxHashMap<String, Level>,
+    _paths: Vec<String>,
 }
 
 pub struct Logger {
     worker_handle: Option<JoinHandle<()>>,
     worker_flag: Arc<AtomicBool>,
+    settings: Arc<Mutex<Settings>>,
     channel: (Sender<InternalRecord>, Receiver<InternalRecord>),
-
+}
+impl Default for Logger {
+    fn default() -> Self {
+        Self::new(Settings::default())
+    }
 }
 impl Logger {
-    pub fn new() -> Self {
+    pub fn new(settings: Settings) -> Self {
         let worker_flag = Arc::new(AtomicBool::new(true));
 
-        let channel: (Sender<InternalRecord>, Receiver<InternalRecord>) = crossbeam_channel::bounded(4096);
+        let channel: (Sender<InternalRecord>, Receiver<InternalRecord>) =
+            crossbeam_channel::bounded(4096);
+
+        let settings = Arc::new(Mutex::new(settings));
 
         let inner_flag = worker_flag.clone();
         let receiver = channel.1.clone();
+        let worker_settings = settings.clone();
+
         let worker_handle = std::thread::spawn(move || {
             while inner_flag.load(Ordering::Relaxed) {
                 while let Ok(event) = receiver.recv() {
@@ -48,16 +65,27 @@ impl Logger {
         Self {
             worker_handle: Some(worker_handle),
             worker_flag,
+            settings,
             channel,
         }
     }
+
+    pub fn change_settings(settings: Settings) {
+        *LOGGER.as_ref().settings.lock() = settings;
+    }
+
     pub fn init() -> Result<(), SetLoggerError> {
         log::set_logger(&*LOGGER.as_ref()).map(|()| log::set_max_level(LevelFilter::Trace))
     }
 }
 impl Log for Logger {
-    fn enabled(&self, _: &Metadata) -> bool {
-        true
+    fn enabled(&self, meta: &Metadata) -> bool {
+        self.settings
+            .lock()
+            .targets
+            .get(meta.target())
+            .map(|level| *level <= meta.level())
+            .unwrap_or(false)
     }
 
     fn log(&self, record: &Record) {
@@ -68,8 +96,8 @@ impl Log for Logger {
                 message: format!("{}", record.args()),
                 module_path: record.module_path().map(|s| s.to_owned()),
                 file: record.file().map(|s| s.to_owned()),
-                line: record.line()
-            });
+                line: record.line(),
+            }).unwrap_or_else(|e| println!("Failed to write channel: {:?}", e) );
         }
     }
 
